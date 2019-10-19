aa62a426c0ac5cfd2b16dd6e434c5ff3b36db630
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2019-10-19 14:50:45 -0600

    aaaaaaaaaaa

--- cogs/Music.py
+++ cogs/Music.py
@@ -51,40 +51,41 @@ class Music(commands.Cog):
     @commands.command()
     async def play(self, ctx, url: str):
 
-        # song_there = os.path.isfile('song.mp3')
-        # try:
-        #     if song_there:
-        #         os.remove('song.mp3')
-        #         #print('removed old song')
-        # except PermissionError:
-        #     #print('tried to delete song but is currently playing')
-        #     await ctx.send('stahp the music before doing another one')
-        #     return
-        #
-        # await ctx.send('Getting things ready now')
-        #
-        #
-        # async with ctx.typing():
+
+        song_there = os.path.isfile('song.mp3')
+        try:
+            if song_there:
+                os.remove('song.mp3')
+                #print('removed old song')
+        except PermissionError:
+            #print('tried to delete song but is currently playing')
+            await ctx.send('stahp the music before doing another one')
+            return
+
+        await ctx.send('Getting things ready now')
+
+
+
         voice = get(self.client.voice_clients, guild=ctx.guild)
-        #
-        #     ydl_opts = {
-        #         'format': 'bestaudio/best',
-        #         'postprocessors': [{
-        #             'key': 'FFmpegExtractAudio',
-        #             'preferredcodec': 'mp3',
-        #             'preferredquality': '192',
-        #         }],
-        #     }
-        #
-        #     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        #         #print("Downloading audio now\n")
-        #         ydl.download([url])
-        #
-        #     for file in os.listdir("././"):
-        #         if file.endswith(".mp3"):
-        #             name = file
-        #             #print(f"Renamed File: {file}\n")
-        #             os.rename(file, "song.mp3")
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            #print("Downloading audio now\n")
+            ydl.download([url])
+
+        for file in os.listdir("././"):
+            if file.endswith(".mp3"):
+                name = file
+                #print(f"Renamed File: {file}\n")
+                os.rename(file, "song.mp3")
 
         voice.play(discord.FFmpegPCMAudio("song.mp3"))
         voice.source = discord.PCMVolumeTransformer(voice.source)
