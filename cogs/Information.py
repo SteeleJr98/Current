858f71c264bb8774f0fe9ab6514128be024d7480
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2019-10-18 23:28:29 -0600

    Updated Help Command and Changed Version Number

--- cogs/Information.py
+++ cogs/Information.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 
 
-Version = discord.Game('SteeleHookv2.4.1')
+Version = discord.Game('Snapshot 2.5.1')
 
 class Information(commands.Cog):
 
@@ -28,56 +28,62 @@ class Information(commands.Cog):
     # async def ping2(self, ctx):
     #     await ctx.send('responding from a cog')
 
+    @commands.command()
+    async def version(self, ctx):
+        await ctx.send(Version)
+
 
     @commands.command()
     async def help(self, ctx):
+        async with ctx.typing():
 
-        embed = discord.Embed(
-            colour = discord.Colour.green()
-        )
-
-
-        embed.set_author(name='Fun')
-        embed.add_field(name='8ball', value='Ask a question and a yes or no responce. Usage: 8ball (question)', inline=False)
-        embed.add_field(name='coinflip', value='Heads or tails? Let the bot decide. Usage: coinflip', inline=False)
-        embed.add_field(name='say', value='Make the bot say anything you like. Usage: say (message)', inline=False)
-
-        await ctx.author.send(embed=embed)
+            embed = discord.Embed(
+                colour = discord.Colour.green()
+            )
 
 
-        embed2 = discord.Embed(
-            colour = discord.Colour.orange()
-        )
+            embed.set_author(name='Fun')
+            embed.add_field(name='8ball', value='Ask a question and a yes or no responce. Usage: 8ball (question)', inline=False)
+            embed.add_field(name='coinflip', value='Heads or tails? Let the bot decide. Usage: coinflip', inline=False)
+            embed.add_field(name='say', value='Make the bot say anything you like. Usage: say (message)', inline=False)
 
-        embed2.set_author(name='Information')
-        embed2.add_field(name='ping', value='Pong! but also shows the bot\'s latency. Usage: ping', inline=False)
-
-        await ctx.author.send(embed=embed2)
+            await ctx.author.send(embed=embed)
 
 
-        embed3 = discord.Embed(
-            colour = discord.Colour.red()
-        )
+            embed2 = discord.Embed(
+                colour = discord.Colour.orange()
+            )
 
-        embed3.set_author(name='Moderation')
-        embed3.add_field(name='ban', value='Bans a member from the serevr. Usage: ban (member ID) {reason}', inline=False)
-        embed3.add_field(name='kick', value='Kicks a member from the server Usage: kick (member ID) {reason}', inline=False)
-        embed3.add_field(name='unban', value='Unbans a memebr from the sevrer. Usage: unban (Member\'s discord tag)', inline=False)
-        embed3.add_field(name='clear', value='Removes a number of messages sent in a channel. Usage: clear (Number of message)', inline=False)
-        embed3.add_field(name='changeprefix', value='Changes the bot\'s prefix for the server. Usage: changeprefix (new prefix)', inline=False)
+            embed2.set_author(name='Information')
+            embed2.add_field(name='ping', value='Pong! but also shows the bot\'s latency. Usage: ping', inline=False)
+            embed2.add_field(name='version', value='Shows the bot\'s current version. Usage: version', inline=False)
 
-        await ctx.author.send(embed=embed3)
+            await ctx.author.send(embed=embed2)
 
 
-        embed4 = discord.Embed(
-            colour = discord.Colour.blue()
-        )
+            embed3 = discord.Embed(
+                colour = discord.Colour.red()
+            )
 
-        embed4.set_author(name='Help')
-        embed4.add_field(name='help', value='sends you this message. Usage: help', inline=False)
+            embed3.set_author(name='Moderation')
+            embed3.add_field(name='ban', value='Bans a member from the serevr. Usage: ban (member ID) {reason}', inline=False)
+            embed3.add_field(name='kick', value='Kicks a member from the server Usage: kick (member ID) {reason}', inline=False)
+            embed3.add_field(name='unban', value='Unbans a memebr from the sevrer. Usage: unban (Member\'s discord tag)', inline=False)
+            embed3.add_field(name='clear', value='Removes a number of messages sent in a channel. Usage: clear (Number of messages)', inline=False)
+            embed3.add_field(name='changeprefix', value='Changes the bot\'s prefix for the server. Usage: changeprefix (new prefix)', inline=False)
 
-        await ctx.author.send(embed=embed4)
-        await ctx.send('Ckeck your DMs')
+            await ctx.author.send(embed=embed3)
+
+
+            embed4 = discord.Embed(
+                colour = discord.Colour.blue()
+            )
+
+            embed4.set_author(name='Help')
+            embed4.add_field(name='help', value='sends you this message. Usage: help', inline=False)
+
+            await ctx.author.send(embed=embed4)
+            await ctx.send('Ckeck your DMs')
 
 
 
