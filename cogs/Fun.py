380e58cfda3a2ec368aa536551374aea4f186c69
SteeleJr98 <54185181+SteeleJr98@users.noreply.github.com>
2019-10-22 21:34:40 -0600

    Added responses for the 8ball command

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -9,7 +9,16 @@ class Fun(commands.Cog):
 
     @commands.command(aliases=['8ball']) #short random choice example with question/response
     async def _8ball(self, ctx, *, question):
-        responses = ['Yes', 'No']
+        responses = [ 'It is certain.', 'It is decidedly so.',
+                    'Without a doubt.', 'Yes - definitely.',
+                    'You may rely on it.', 'As I see it, yes.',
+                    'Most likely.', 'Outlook good.',
+                    'Yes.', 'Signs point to yes.',
+                    'Reply hazy, try again.', 'Ask again later.',
+                    'Better not tell you now.', 'Cannot predict now.',
+                    'Concentrate and ask again.', 'Don\'t count on it.',
+                    'My reply is no.', 'My sources say no.',
+                    'Outlook not so good.', 'Very doubtful.']
         await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 
